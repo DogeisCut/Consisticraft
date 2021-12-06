e75a1f7f0b756a354fc232cbf4a3b4bd7e68e5c3
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2021-12-05 21:02:43 -0700

    Fix capitals

--- src/main/java/com/rickisparks/consisticraft/Consisticraft.java
+++ src/main/java/com/rickisparks/consisticraft/Consisticraft.java
@@ -1,34 +1,25 @@
 package com.rickisparks.consisticraft;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(consisticraft.MOD_ID)
-public class consisticraft {
+@Mod(Consisticraft.MOD_ID)
+public class Consisticraft {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "consisticraft";
 
-    public consisticraft() {
+    public Consisticraft() {
 
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
+
         eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
